6831ffbc2949c804ee79962d00897e6f60f48da9
Katia Reis <62106987+katiareis@users.noreply.github.com>
2021-12-28 13:42:35 -0300

    Update JavaScript code
    Changed JS code with things I have been learning.
    Applied variables and used CSS classes to call function.

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-// Selecting the keys
+// Selecting only key "one"
 /* const one = document.querySelector(".one");
 one.addEventListener("click", (event) => {
   console.log("hello");
 });
 */
+
 const calculator = document.querySelector(".calculator");
 const display = calculator.querySelector(".calculator-display");
-const keys = calculator.querySelector(".calculator-keys");
+const keys = calculator.querySelector(".calculator-keys"); //Selects all the keys
 
+//Selects all the keys
 keys.addEventListener("click", (event) => {
+  //Narrow the selection
   if (!event.target.closest("button")) return;
 
-  const key = event.target; //seleciona todo o elemento
-  const keyValue = key.textContent; //seleciona o conteúdo(string) do elemento
-  const displayValue = display.textContent;
+  const key = event.target; //selects complete html tag
+  const keyValue = key.textContent; //selects the content the the tag(str)
+  const displayValue = display.textContent; //selects the content of the display
 
   // Is this a number key?
   if (key.classList.contains("number")) {
+    //Check if the input is not the placeholder 0
     if (displayValue === "0") {
-      display.textContent = keyValue;
+      display.textContent = keyValue; //Shows the tag content in the display
+    } else if (keyValue == "C") {
+      display.textContent = "0"; //Clear the contents
     } else {
-      display.textContent = displayValue + keyValue;
+      display.textContent = displayValue + keyValue; //Shows input numbers in the display concatened(str)
     }
   }
 
-  //Is this an operator key?
-  key.dataset.key === "operator";
-  key.getAttribute("data-key-type") === "operator";
+  // Is this an operator key?
   if (key.classList.contains("operator")) {
-    console.log(key);
-
-    calculator.dataset.previousKeyType = "operator";
+    display.textContent = displayValue + keyValue;
+  } else if ((keyValue !== "") & (keyValue === "=")) {
+    display.textContent = eval(displayValue);
   }
 });
-
-// Selecting the keys
